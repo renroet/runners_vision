c18950919d63b33900a23f749701e48a5c48a348
Ren Roetzel <renroetzel@gmail.com>
2023-07-03 12:22:49 -0400

    updated schema

--- app.py
+++ app.py
@@ -13,6 +13,11 @@
 #   """Renders an Index template that changes with the user filter search"""
 #   """Form style search bar like in homepage"
 
+# app.route('/races/<int:race_id>)
+# def show_race_info():
+#   """Shows all information about a race from the API"""
+#   """This is where user can select race to be on their vision board"""
+
 # app.route('/login')
 #   """Need and authentication in the user model"""
 
@@ -30,7 +35,7 @@
 #   """Flashes occur at important times to inform user about upcoming moments and training"""
 #   """Spot for weekly sum of miles run may also be good"""
 
-# app.route('/race/<int:user_race_id>')
+# app.route('/race/<int:users_races_id>')
 #   """If a user has selected a race as active, then they can log their runs for that race
 #   and keep track of their trainin"""
 #   """It will also have a countdown "
@@ -38,15 +43,28 @@
 #   training. Users can also click the days (tiles) of the calender to add training they 
 #   have completed -- app.route('/race/<int:user_race_id>/training/add) """
 
-# app.route('/race/<int:user_race_id>/training/add)
+# app.route('api/race/<int:users_races_id>/training/add)
 #   """training can also be deleted if added in error"""
 
 # ############ AUTHENTICATION SHOULD BE IMPLIMENTED FOR ALL ADDED/DELETED TRAINING ############
 
+# app.route('api/race/<int:users_races_id>/training/delete)
+#   """training can also be deleted if added in error"""
 
+# #### trainings will be validated and added into db using wtforms ######
 
+# app.route('api/race/<int:users_races_id>')......or maybe just an UPDATE PATCH
+# """PATCH to update to completed or inactive/active"
 
-
+# make api to: 
+#   - create "users_races"
+#   - update "users_races" as completed, active, or inactive
+#   - delete "users_races"
+#   - create "users_races" - "training"
+#   - delete "users_races" - "training"
+#   - create "user"
+#   - update "user" to private/public
+#   - delete "user"
 
 
 
